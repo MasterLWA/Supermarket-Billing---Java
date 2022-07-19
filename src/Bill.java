35a9c6b957a73d81460d8e1527a9ad6b1d83f0f0
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-07-20 01:03:15 +0530

    Add files via upload

---
+++ src/Bill.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Bill {
+    private int billID;
+    private  float totalPrice;
+    private Date date;
+
+
+    public float calTotal() {
+
+        return 0;
+    }
+
+}
---
+++ src/item.java
@@ -0,0 +1,13 @@
+public class item {
+    private String name;
+    private String brand;
+    private float price;
+    private int quantity;
+
+    public void setPrice(String ibrnd,String iname, float iPrice, int qtys){
+        name = iname;
+        brand = ibrnd;
+        price = iPrice;
+        quantity = qtys;
+    }
+}
