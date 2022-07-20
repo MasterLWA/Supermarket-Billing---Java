3d6b4afec9c2b8cf932891e0cb47a91457a5a909
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-07-20 15:14:25 +0530

    Add files via upload

--- src/Bill.java
+++ src/Bill.java
@@ -4,11 +4,17 @@ public class Bill {
     private int billID;
     private  float totalPrice;
     private Date date;
+    //private String *items[];
 
 
     public float calTotal() {
 
         return 0;
+    }
+
+    public void displayTotal(){
+        System.out.println("Total:"+ totalPrice);
+        System.out.println("Bill Number: " + billID);
     }
 
 }
